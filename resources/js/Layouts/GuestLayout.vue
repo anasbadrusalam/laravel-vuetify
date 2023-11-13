87e3faac857d11db5ae1b1f71cb844a2eda7fb64
anasbadrusalam <anasbadrusalam@gmail.com>
2023-11-13 22:36:11 +0700

    WIP

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,14 +1,25 @@
 <template>
     <v-app>
         <v-app-bar density="comfortable" border flat>
-            <v-app-bar-title>Laravel</v-app-bar-title>
-            <v-spacer></v-spacer>
-            <v-btn color="primary" variant="flat">
-                Masuk
-            </v-btn>
+            <v-container>
+                <v-row align="center">
+                    <v-col>
+                        <v-app-bar-title>
+                            <Link as="button" :href="route('welcome')">
+                                Laravel
+                            </Link>
+                        </v-app-bar-title>
+                    </v-col>
+                    <v-col class="text-right">
+                        <Link :href="route('login')">
+                            <v-btn color="primary" variant="flat"> Masuk </v-btn>
+                        </Link>
+                    </v-col>
+                </v-row>
+            </v-container>
         </v-app-bar>
         <v-main class="bg-grey-lighten-3">
-            <v-container fluid>
+            <v-container>
                 <slot />
             </v-container>
         </v-main>
@@ -16,7 +27,13 @@
 </template>
 
 <script>
-export default {};
+import { Link } from "@inertiajs/vue3";
+
+export default {
+    components: {
+        Link,
+    },
+};
 </script>
 
 <style></style>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,22 +1,29 @@
 <template>
-  <GuestLayout>
-    <v-card flat>
-        <v-card-text>
-            hii
-        </v-card-text>
-    </v-card>
-  </GuestLayout>
+    <Head title="Masuk"></Head>
+    <GuestLayout>
+        <v-sheet height="100" >
+            <v-row align="center" justify="center">
+                <v-col>
+                    <v-card flat>
+                        <v-card-text> hii </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-sheet >
+    </GuestLayout>
 </template>
 
 <script>
-import GuestLayout from '@/Layouts/GuestLayout.vue'
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import { Head, Link } from "@inertiajs/vue3";
+
 export default {
-    components:{
-        GuestLayout
-    }
-}
+    components: {
+        GuestLayout,
+        Head,
+        Link,
+    },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
