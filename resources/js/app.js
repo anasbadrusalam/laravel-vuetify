e82578e9c4b830a5516a9035a17e7fa8a926f919
anasbadrusalam <anasbadrusalam@gmail.com>
2023-11-13 14:21:59 +0700

    WIP

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,11 @@
 import './bootstrap';
-import '../css/app.css';
+// import '../css/app.css';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import vuetify from './Plugins/vuetify';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -15,6 +16,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(vuetify)
             .mount(el);
     },
     progress: {
