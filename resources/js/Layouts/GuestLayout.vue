ea4642d6cca541f053d3a2f416fc93d0437e7192
anasbadrusalam <anasbadrusalam@gmail.com>
2023-11-13 14:39:57 +0700

    WIP

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,7 +1,20 @@
 <template>
     <v-app>
-        <v-main>
-            <slot />
+        <v-app-bar density="comfortable" border flat>
+            <v-app-bar-title>Laravel</v-app-bar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-btn color="primary" variant="flat">
+                Masuk
+            </v-btn>
+        </v-app-bar>
+        <v-main class="bg-grey-lighten-3">
+            <v-container fluid>
+                <slot />
+            </v-container>
         </v-main>
     </v-app>
 </template>
